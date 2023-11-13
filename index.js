b0ba2bd01f55b1e2510abc651a7a909d8b576aa0
Developer <106108668+Jaun-Raza@users.noreply.github.com>
2023-11-13 13:23:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const app = express();
 const port = 5000;
 
 app.use((req, res, next) => {
-    // res.setHeader("Access-Control-Allow-Origin", "https://cooltalks.vercel.app");
-    // res.header(
-    //     "Access-Control-Allow-Headers",
-    //     "Origin, X-Requested-With, Content-Type, Accept"
-    // );
-  res.header('Access-Control-Allow-Origin', 'https://cooltalks.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  // next();
+    try {
+        res.header('Access-Control-Allow-Origin', 'https://cooltalks.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
+    }} catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+}
 })
 app.use(express.json({limit: '5024mb'}));
 app.use(express.urlencoded({ extended: true }));
